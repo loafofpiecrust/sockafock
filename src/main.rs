d47d4926dd62302eb9ca4edc3aabd31f9f363cf8
Taylor Snead <taylorsnead@gmail.com>
2017-06-22 22:36:47 +0000

    Clean up the main file a bit. Add README.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 #![feature(lookup_host)]
 
-extern crate futures;
-extern crate tokio_core;
-extern crate tokio_io;
-extern crate socks;
 extern crate byteorder;
 
 use std::io::{Read, Write};
@@ -14,91 +10,75 @@ use byteorder::{BigEndian, ReadBytesExt};
 use std::io::{Cursor};
 
 fn main() {
-    let port = 1080;
+    let port = 1080; // proxy port for clients to connect to
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
 
+    // for each connection
     for client in listener.incoming() {
         match client {
             Ok(mut client) => {
+                // TODO: Use a taskpool?
+                // Spawn a thread to handle each new connection.
                 thread::spawn(move || {
-                    // Connection!
                     let peer = client.peer_addr().unwrap();
-                    println!("connected to {:?}", peer);
+                    println!("Connected to {:?}", peer);
                     let data = &mut [0];
 
+                    // Socks version
                     client.read(data).unwrap();
-                    println!("socks version: {:?}", data);
+                    if data[0] != 5 {
+                        // TODO: Error more gracefully?
+                        panic!("Only SOCKS5 is supported. No SOCKS4 or anything else.");
+                    }
 
-                    // new connection
+                    // Authentication data
                     client.read(data).unwrap();
                     let auth_count = data[0];
-                    println!("# auth methods: {:?}", auth_count);
+                    // println!("# of auth methods: {:?}", auth_count);
 
-                    let mut login = false;
-                    for i in 0..auth_count {
-                        client.read(data).unwrap();
-                        println!("auth method {}: {:?}", i + 1, data);
-                        // 0 = no auth
-                        // 1 = GSSAPI
-                        // 2 = user/pass
-                        if data[0] == 2 {
-                            login = true;
-                        }
-                    }
+                    let mut auth_methods = vec![0; auth_count as usize];
+                    client.read(&mut auth_methods).unwrap();
+                    // TODO: Handle other types of auth besides NONE.
 
-                    let written = client.write(&[5]).unwrap();
-                    client.write(&[0]).unwrap();
-                    client.flush().unwrap();
-                    println!("sent {:?} bytes", written);
-                    if !login {
-                    } else {
-                        // TODO: Add login capacity here!
-                    }
+                    client.write(&[5, 0]).unwrap();
+                    // TODO: Add login capacity here!
 
 
                     client.read(data).unwrap(); // socks ver again
                     client.read(data).unwrap();
                     let command = data[0];
-                    println!("command {:?}", command);
+                    // println!("command {:?}", command);
                     if command != 1 {
                         // Doesn't want to make a TCP connection!
+                        // TODO: Handle more elegantly
                         return;
                     }
 
-                    client.read(data).unwrap(); // Must be 0
-                    println!("must be 0? is {:?}", data);
+                    client.read(data).unwrap(); // Reserved value, "must be 0" supposedly
                     client.read(data).unwrap();
-                    let addr_ty = data[0];
-                    println!("addr ty {:?}", addr_ty);
-                    let port = &mut [0, 0];
-                    let remote_host = match addr_ty {
+                    let addr_type = data[0];
+                    let port = &mut [0, 80];
+                    let remote_host = match addr_type {
                         1 => { // IPv4 address
                             let ip = &mut [0, 0, 0, 0];
                             client.read(ip).unwrap();
-                            println!("going to ip {:?}", ip);
                             client.read(port).unwrap();
-                            // How to use port?
-                            println!("port! {:?}", port);
                             SocketAddr::new(IpAddr::V4(Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3])), 80)
                         },
-                        3 => { // Domain name
-                            let len = &mut [0];
-                            client.read(len).unwrap();
-                            let mut url = vec![0; len[0] as usize];
-                            client.read(&mut url).unwrap();
-                            let url = String::from_utf8(url).unwrap();
-                            println!("going to addr {:?}", url);
+                        3 => { // Domain name (URL)
+                            client.read(data).unwrap(); // length of url
+                            let mut url = vec![0; data[0] as usize]; // remote url buffer
+                            client.read(&mut url).unwrap(); // retrieve remote url
                             client.read(port).unwrap();
-                            // How to use port?
-                            println!("port! {:?}", port);
-                            let host = net::lookup_host(&url).unwrap().next().unwrap();
-                            println!("at host {:?}", host);
+
+                            let url = String::from_utf8(url).unwrap();
+                            let mut host = net::lookup_host(&url).unwrap().next().unwrap();
+                            host.set_port(80);
 
                             host
                         },
                         _ => { // IPv6 address
-                            println!("foodled");
-                            unimplemented!()
+                            unimplemented!("IPv6 addresses not yet supported.")
                         }
                     };
 
@@ -107,18 +87,17 @@ fn main() {
                     client.write(&[5, 0, 0, 1]).unwrap(); // Always IPv4 addr for now
                     match remote_host {
                         SocketAddr::V4(sock) => {
+                            // Send the (DNS) resolved remote ip address.
                             client.write(&sock.ip().octets()).unwrap();
                         }
                         _ => unimplemented!()
                     }
-                    client.write(port).unwrap(); // server port
+                    client.write(port).unwrap(); // remote port
 
-                    client.flush().unwrap();
+                    client.flush().unwrap(); // make for sure everything is sent
 
-                    let port_num = {
-                        let mut rdr = Cursor::new(&port);
-                        rdr.read_u16::<BigEndian>().unwrap()
-                    };
+                    // Convert server-order (almost always big endian) u8's to a u16
+                    let port_num = Cursor::new(&port).read_u16::<BigEndian>().unwrap();
 
                     match remote_host {
                         SocketAddr::V4(sock) => {
